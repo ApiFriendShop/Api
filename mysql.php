00d859a2776ad487a5388ad8d6a30b78376dfbad
YasinBawar <153268742+YasinBawar@users.noreply.github.com>
2023-12-12 13:09:44 +0430

    Update mysql.php

--- mysql.php
+++ mysql.php
@@ -1,1 +1,12 @@
+<?php
 
+function connect_db(){
+    try {
+        $conn = new PDO("mysql:host=localhost;dbname=Shopping","root" ,"" );
+    } catch(PDOException $e) {
+     echo   $e->getMessage();
+    }
+    return $conn;
+}
+
+?>
