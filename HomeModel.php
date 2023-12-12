99f8e8e31f0bd21cd8f6050eb8efa99c795d9c02
YasinBawar <153268742+YasinBawar@users.noreply.github.com>
2023-12-12 13:10:25 +0430

    Update HomeModel.php

--- HomeModel.php
+++ HomeModel.php
@@ -1,1 +1,60 @@
+<?php
+include_once 'mysql.php';
+
+function findSliders(){
+    $conn=connect_db();
+    $query=$conn->prepare("SELECT id,Title,img FROM slider where status='on' ");
+    $query->execute();
+    $rez=$query->fetchAll(PDO::FETCH_OBJ);
+    return $rez;
+}
+function findPoster(){
+    $conn=connect_db();
+    $query=$conn->prepare("SELECT * FROM Posters where status='on'");
+    $query->execute();
+    $rez=$query->fetchAll(PDO::FETCH_OBJ);
+    return $rez;
+}
+function findCartFirst(){
+    $conn=connect_db();
+    $query=$conn->prepare("SELECT * FROM cart_first ");
+    $query->execute();
+    $rez=$query->fetchAll(PDO::FETCH_OBJ);
+    return $rez;
+}
+function findCart(){
+    $conn=connect_db();
+    $query=$conn->prepare("SELECT * FROM cart");
+    $query->execute();
+    $rez=$query->fetchAll(PDO::FETCH_OBJ);
+    return $rez;
+}
+function findCatgory(){
+    $conn=connect_db();
+    $query=$conn->prepare("SELECT * FROM catgory ");
+    $query->execute();
+    $rez=$query->fetchAll(PDO::FETCH_OBJ);
+    return $rez;
+}
+function findComments(){
+    $conn=connect_db();
+    $query=$conn->prepare("SELECT * FROM coment ");
+    $query->execute();
+    $rez=$query->fetchAll(PDO::FETCH_OBJ);
+    return $rez;
+}
+function findLives(){
+    $conn=connect_db();
+    $query=$conn->prepare("SELECT * FROM liked ");
+    $query->execute();
+    $rez=$query->fetchAll(PDO::FETCH_OBJ);
+    return $rez;
+}
+function findUsers(){
+    $conn=connect_db();
+    $query=$conn->prepare("SELECT * FROM user");
+    $query->execute();
+    $rez=$query->fetchAll(PDO::FETCH_OBJ);
+    return $rez;
+}
 
